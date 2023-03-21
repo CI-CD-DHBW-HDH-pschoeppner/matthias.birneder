37564d130519e580002c5a2bacba9fa0dac67540
matthias birneder <77612563+matthias031299@users.noreply.github.com>
2023-03-21 09:41:27 +0100

    init

--- src/logic/todo.ts
+++ src/logic/todo.ts
@@ -8,8 +8,13 @@ export class TodoItem {
 
 // this function must return a unique ID every time it is called
 export function generateID(): string {
-    // TODO: implement
-    return ""
+  // TODO: implement
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let result = "";
+  for (let i = 0; i < 5; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
 }
 
 // make sure, that
@@ -18,24 +23,41 @@ export function generateID(): string {
 // the todo isn't contained in the todos array (case insensitive)
 export function validateTodo(todo: TodoItem, todos: TodoItem[]): boolean {
   // TODO: implement
-  return false;
+  if (todo.value.length > 255) {
+    return false;
+  }
+  if (todo.value.trim().length === 0) {
+    return false;
+  }
+  const existingTodo = todos.find(
+    (t) => t.value.toLowerCase() === todo.value.toLowerCase()
+  );
+  if (existingTodo) {
+    return false;
+  }
+  return true;
 }
 
 // capitalize the first letter of the todo
 export function formatTodo(todo: TodoItem): TodoItem {
   // TODO: implement
+  const formattedValue =
+    todo.value.charAt(0).toUpperCase() + todo.value.slice(1);
   return {
-    id: "",
-    value: "",
-    done: false
-  }
+    id: todo.id,
+    value: formattedValue,
+    done: todo.done,
+  };
 }
 
 // generate a random rgb color
 // each value (r,g,b) should be between 50 and 150
 export function generateColor(): string {
   // TODO: implement
-  return ""
+  const r = Math.floor(Math.random() * 101) + 50;
+  const g = Math.floor(Math.random() * 101) + 50;
+  const b = Math.floor(Math.random() * 101) + 50;
+  return "rgb(" + String(r) + "," + String(g) + "," + String(b) + ")";
 }
 
 export const todoList = writable<TodoItem[]>([]);
